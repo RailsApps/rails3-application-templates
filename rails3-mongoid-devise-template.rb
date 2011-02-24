4ea87a921f11db90083a9380c49e46f34b4b0641
Daniel Kehoe <kehoe@fortuity.com>
2011-02-24 15:20:46 -0800

    remove long gitignore code from the git recipe and get it from a file instead

--- rails3-mongoid-devise-template.rb
+++ rails3-mongoid-devise-template.rb
@@ -61,63 +61,7 @@ say_recipe 'Git'
 
 # Git should ignore some files
 remove_file '.gitignore'
-file '.gitignore', <<-'IGNORES'.gsub(/^ {2}/, '')
-  # bundler state
-  /.bundle
-  /vendor/bundle/
-
-  # minimal Rails specific artifacts
-  db/*.sqlite3
-  /log/*
-  tmp/*
-
-  # various artifacts
-  **.war
-  *.rbc
-  *.sassc
-  .rspec
-  .sass-cache
-  /config/config.yml
-  /config/database.yml
-  /coverage.data
-  /coverage/
-  /db/*.javadb/
-  /db/*.sqlite3-journal
-  /doc/api/
-  /doc/app/
-  /doc/features.html
-  /doc/specs.html
-  /log/*
-  /public/cache
-  /public/stylesheets/compiled
-  /public/system
-  /spec/tmp/*
-  /tmp/*
-  /cache
-  /capybara*
-  /capybara-*.html
-  /gems
-  /rerun.txt
-  /spec/requests
-  /spec/routing
-  /spec/views
-  /specifications
-
-  # scm revert files
-  **.orig
-
-  # mac finder poop
-  .DS_Store
-
-  # netbeans project directory
-  /nbproject/
-
-  # textmate project files
-  /*.tmpproj
-
-  # vim poop
-  **.swp
-IGNORES
+get "https://github.com/fortuity/rails-template-recipes/raw/master/gitignore.txt", ".gitignore"
 
 # Initialize new Git repo
 git :init
