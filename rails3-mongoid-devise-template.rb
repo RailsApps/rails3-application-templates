8b3f78d4dd10737542ea2d6b785993491ad2a605
Daniel Kehoe <kehoe@fortuity.com>
2011-02-23 18:41:34 -0800

    remove superfluous Haml application layout

--- rails3-mongoid-devise-template.rb
+++ rails3-mongoid-devise-template.rb
@@ -664,26 +664,6 @@ CSS
 
 end
 
-if recipe_list.include? 'haml'
-  say_recipe 'Application layout (Haml)'
-
-  remove_file 'app/views/layouts/application.html.erb'
-
-  create_file 'app/views/layouts/application.html.haml' do
-    <<-FLASHES.gsub(/^ {6}/, '')
-      !!! 5
-      %html
-        %head
-          %title #{app_name}
-          = stylesheet_link_tag :all
-          = javascript_include_tag :defaults
-          = csrf_meta_tag
-        %body
-          = yield
-    FLASHES
-  end
-end
-
 # >--------------------------------[ application_layout ]--------------------------------<
 
 # Application template recipe. Check for a newer version here:
