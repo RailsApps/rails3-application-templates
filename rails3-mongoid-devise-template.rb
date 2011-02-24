28b5504945d4a41e1a0c7369b7120805a4d1a109
Daniel Kehoe <kehoe@fortuity.com>
2011-02-23 17:01:49 -0800

    for the jQuery recipe, remove the Prototype adapter file

--- rails3-mongoid-devise-template.rb
+++ rails3-mongoid-devise-template.rb
@@ -162,6 +162,9 @@ if recipe_list.include? 'jquery'
   # Adds the latest jQuery and Rails UJS helpers for jQuery.
   say_recipe 'jQuery'
 
+  # remove the Prototype adapter file
+  remove_file 'public/javascripts/rails.js'
+  # add jQuery files
   inside "public/javascripts" do
     get "https://github.com/rails/jquery-ujs/raw/master/src/rails.js", "rails.js"
     get "http://code.jquery.com/jquery-1.5.min.js", "jquery.js"
