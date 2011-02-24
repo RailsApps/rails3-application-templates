35c7777abf90931e45f85754b4d6d5280b8b7b8b
Daniel Kehoe <kehoe@fortuity.com>
2011-02-23 18:31:38 -0800

    patched jQuery recipe

--- rails3-mongoid-devise-template.rb
+++ rails3-mongoid-devise-template.rb
@@ -174,12 +174,9 @@ if recipe_list.include? 'jquery'
     get "https://github.com/rails/jquery-ujs/raw/master/src/rails.js", "rails.js"
     get "http://code.jquery.com/jquery-1.5.min.js", "jquery.js"
   end
-
-  application do
-    "\nconfig.action_view.javascript_expansions[:defaults] = %w(jquery rails)\n"
-  end
-
-  gsub_file "config/application.rb", /        config.action_view.javascript_expansions\[:defaults\] = \%w\(\)\n/, ""
+  # adjust the Javascript defaults
+  inject_into_file 'config/application.rb', "config.action_view.javascript_expansions[:defaults] = %w(jquery rails)\n", :after => "config.action_view.javascript_expansions[:defaults] = %w()\n", :verbose => false
+  gsub_file "config/application.rb", /config.action_view.javascript_expansions\[:defaults\] = \%w\(\)\n/, ""
   
 end 
 
