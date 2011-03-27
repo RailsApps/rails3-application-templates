c41463da323c29d189f2f441d10ee26301a6b354
Daniel Kehoe <kehoe@fortuity.com>
2011-03-27 15:07:02 -0700

    back out workaround added to resolve Issue 17, should be no problem using any Rubygems version newer than 1.53 (2011-02-26)

--- rails3-mongoid-devise-template.rb
+++ rails3-mongoid-devise-template.rb
@@ -99,17 +99,6 @@ if recipes.include? 'mongoid'
   gem 'mongoid', '>= 2.0.0.rc.7'
   gem 'bson_ext', '>= 1.2.4'
 
-  #----------------------------------------------------------------------------
-  # Resolve issue 17: https://github.com/fortuity/rails3-mongoid-devise/issues#issue/17
-  # Change YAML Engine to accommodate Ruby 1.9.2p180 yaml parser problem.
-  # Rubygems 1.5.0 changes the yaml parsing default from syck 
-  # to psych and psych doesn't like the ":<<" in yaml files.
-  # This is a workaround until the next version of Ruby is published.
-  # http://groups.google.com/group/mongoid/browse_thread/thread/9213a17a73d3c422
-  # http://redmine.ruby-lang.org/issues/show/4300
-  #----------------------------------------------------------------------------                         
-  inject_into_file 'config/environment.rb', "\nrequire 'yaml'\nYAML::ENGINE.yamler= 'syck'\n", :after => "require File.expand_path('../application', __FILE__)", :verbose => false
-
   # generate mongoid configuration
   after_bundler do
     generate 'mongoid:config'
