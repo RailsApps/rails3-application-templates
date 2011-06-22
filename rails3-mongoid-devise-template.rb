26534ce70af5c2f55d8077c6979b4379cd8c7cca
Daniel Kehoe <kehoe@fortuity.com>
2011-06-21 18:32:19 -0700

    update templates to include HTML5 Boilerplate option

--- rails3-mongoid-devise-template.rb
+++ rails3-mongoid-devise-template.rb
@@ -1,6 +1,6 @@
 # Application Generator Template
 # Modifies a Rails app to use Mongoid and Devise
-# Usage: rails new APP_NAME -m https://github.com/RailsApps/rails3-application-templates/raw/master/rails3-mongoid-devise-template.rb -T -O -J
+# Usage: rails new APP_NAME -m https://github.com/RailsApps/rails3-application-templates/raw/master/rails3-mongoid-devise-template.rb -T -O
 
 # Information and a tutorial: 
 # http://github.com/RailsApps/rails3-mongoid-devise/
@@ -41,7 +41,7 @@ Rails.application.config.generators do |g|
 end
 RUBY
 
-@recipes = ["jquery", "haml", "rspec", "cucumber", "mongoid", "action_mailer", "devise", "add_user", "home_page", "home_page_users", "seed_database", "users_page", "css_setup", "application_layout", "devise_navigation", "cleanup", "ban_spiders", "git"]
+@recipes = ["jquery", "haml", "rspec", "cucumber", "mongoid", "action_mailer", "devise", "add_user", "home_page", "home_page_users", "seed_database", "users_page", "css_setup", "application_layout", "html5", "devise_navigation", "cleanup", "ban_spiders", "extras", "git"]
 
 def recipes; @recipes end
 def recipe?(name); @recipes.include?(name) end
@@ -362,13 +362,13 @@ if config['cucumber']
   if recipes.include? 'rails 3.0'
     # for Rails 3.0, use only gem versions we know that work
     gem 'cucumber-rails', '0.5.1', :group => :test
-    gem 'capybara', '1.0.0.beta1', :group => :test
+    gem 'capybara', '1.0.0', :group => :test
     gem 'database_cleaner', '0.6.7', :group => :test
     gem 'launchy', '0.4.0', :group => :test
   else
     # for Rails 3.1+, use optimistic versioning for gems
-    gem 'cucumber-rails', '>= 0.5.2', :group => :test
-    gem 'capybara', '>= 1.0.0.rc1', :group => :test
+    gem 'cucumber-rails', '>= 1.0.0', :group => :test
+    gem 'capybara', '>= 1.0.0', :group => :test
     gem 'database_cleaner', '>= 0.6.7', :group => :test
     gem 'launchy', '>= 0.4.0', :group => :test
   end
@@ -948,10 +948,41 @@ ul.hmenu li {
 }
 
 CSS
+
+  # Add a stylesheet for use with HTML5 Boilerplate
+  css_boilerplate = <<-CSS
+
+header nav ul {
+  list-style: none;
+  margin: 0 0 2em;
+  padding: 0;
+}
+header nav ul li {
+  display: inline;
+}
+#flash_notice, #flash_alert {
+  padding: 5px 8px;
+  margin: 10px 0;
+}
+#flash_notice {
+  background-color: #CFC;
+  border: solid 1px #6C6;
+}
+#flash_alert {
+  background-color: #FCC;
+  border: solid 1px #C66;
+}
+
+CSS
+
   if recipes.include? 'rails 3.0'
     create_file 'public/stylesheets/application.css', css
   else
-    append_file 'app/assets/stylesheets/application.css', css
+    if recipes.include? 'html5'
+      append_file 'app/assets/stylesheets/application.css', css_boilerplate
+    else
+      append_file 'app/assets/stylesheets/application.css', css
+    end
   end
 
 end
@@ -976,6 +1007,7 @@ after_bundler do
   # Set up the default application layout
   if recipes.include? 'haml'
     remove_file 'app/views/layouts/application.html.erb'
+    remove_file 'app/views/layouts/application.html.haml'
     # There is Haml code in this script. Changing the indentation is perilous between HAMLs.
     create_file 'app/views/layouts/application.html.haml' do <<-HAML
 !!! 5
@@ -997,15 +1029,166 @@ HAML
       gsub_file 'app/views/layouts/application.html.haml', /csrf_meta_tags/, 'csrf_meta_tag'
     end
   else
-    inject_into_file 'app/views/layouts/application.html.erb', :after => "<body>\n" do
-  <<-ERB
+    unless recipes.include? 'html5'
+      inject_into_file 'app/views/layouts/application.html.erb', :after => "<body>\n" do
+    <<-ERB
   <%- flash.each do |name, msg| -%>
     <%= content_tag :div, msg, :id => "flash_\#{name}" if msg.is_a?(String) %>
   <%- end -%>
 ERB
+      end
     end
   end
 
+end
+
+
+# >---------------------------------[ html5 ]---------------------------------<
+
+@current_recipe = "html5"
+@before_configs["html5"].call if @before_configs["html5"]
+say_recipe 'html5'
+
+config = {}
+config['html5'] = yes_wizard?("Would you like to install HTML5 Boilerplate?") if true && true unless config.key?('html5')
+config['css_option'] = multiple_choice("How do you like your CSS?", [["Normalize CSS for consistent styling across browsers", "normalize"], ["Completely reset all CSS to eliminate styling", "reset"]]) if true && true unless config.key?('css_option')
+@configs[@current_recipe] = config
+
+# Application template recipe for the rails_apps_composer. Check for a newer version here:
+# https://github.com/RailsApps/rails_apps_composer/blob/master/recipes/html5.rb
+
+if config['html5']
+  if recipes.include? 'rails 3.1'
+    after_bundler do
+      say_wizard "HTML5 Boilerplate recipe running 'after bundler'"
+      # Download HTML5 Boilerplate JavaScripts
+      get "https://raw.github.com/paulirish/html5-boilerplate/master/js/libs/modernizr-2.0.min.js", "app/assets/javascripts/modernizr.js"
+      get "https://raw.github.com/paulirish/html5-boilerplate/master/js/libs/respond.min.js", "app/assets/javascripts/respond.js"
+      # Download stylesheet to normalize or reset CSS
+      case config['css_option']
+        when 'normalize'
+          get "https://raw.github.com/necolas/normalize.css/master/normalize.css", "app/assets/stylesheets/normalize.scss"
+        when 'reset'
+          get "https://raw.github.com/paulirish/html5-boilerplate/master/css/style.css", "app/assets/stylesheets/reset.scss"
+      end
+      # Download HTML5 Boilerplate Site Root Assets
+      get "https://raw.github.com/paulirish/html5-boilerplate/master/apple-touch-icon-114x114-precomposed.png", "public/apple-touch-icon-114x114-precomposed.png"
+      get "https://raw.github.com/paulirish/html5-boilerplate/master/apple-touch-icon-57x57-precomposed.png", "public/apple-touch-icon-57x57-precomposed.png"
+      get "https://raw.github.com/paulirish/html5-boilerplate/master/apple-touch-icon-72x72-precomposed.png", "public/apple-touch-icon-72x72-precomposed.png"
+      get "https://raw.github.com/paulirish/html5-boilerplate/master/apple-touch-icon-precomposed.png", "public/apple-touch-icon-precomposed.png"
+      get "https://raw.github.com/paulirish/html5-boilerplate/master/apple-touch-icon.png", "public/apple-touch-icon.png"
+      get "https://raw.github.com/paulirish/html5-boilerplate/master/crossdomain.xml", "public/crossdomain.xml"
+      get "https://raw.github.com/paulirish/html5-boilerplate/master/humans.txt", "public/humans.txt"
+      # Set up the default application layout
+      if recipes.include? 'haml'
+        # create some Haml helpers
+        # We have to use single-quote-style-heredoc to avoid interpolation.
+        inject_into_file 'app/helpers/application_helper.rb', :after => "ApplicationHelper\n" do <<-'RUBY'
+  # Create a named haml tag to wrap IE conditional around a block
+  # http://paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither
+  def ie_tag(name=:body, attrs={}, &block)
+    attrs.symbolize_keys!
+    haml_concat("<!--[if lt IE 7]> #{ tag(name, add_class('ie6', attrs), true) } <![endif]-->".html_safe)
+    haml_concat("<!--[if IE 7]>    #{ tag(name, add_class('ie7', attrs), true) } <![endif]-->".html_safe)
+    haml_concat("<!--[if IE 8]>    #{ tag(name, add_class('ie8', attrs), true) } <![endif]-->".html_safe)
+    haml_concat("<!--[if gt IE 8]><!-->".html_safe)
+    haml_tag name, attrs do
+      haml_concat("<!--<![endif]-->".html_safe)
+      block.call
+    end
+  end
+
+  def ie_html(attrs={}, &block)
+    ie_tag(:html, attrs, &block)
+  end
+
+  def ie_body(attrs={}, &block)
+    ie_tag(:body, attrs, &block)
+  end
+
+private
+
+  def add_class(name, attrs)
+    classes = attrs[:class] || ''
+    classes.strip!
+    classes = ' ' + classes if !classes.blank?
+    classes = name + classes
+    attrs.merge(:class => classes)
+  end
+RUBY
+        end
+        # Haml version of default application layout
+        remove_file 'app/views/layouts/application.html.erb'
+        remove_file 'app/views/layouts/application.html.haml'
+        # There is Haml code in this script. Changing the indentation is perilous between HAMLs.
+        create_file 'app/views/layouts/application.html.haml' do <<-HAML
+- ie_html :lang => 'en', :class => 'no-js' do
+  %head
+    %title #{app_name}
+    = stylesheet_link_tag :application
+    = javascript_include_tag :application
+    = csrf_meta_tags
+    %body
+      #container
+        %header
+          - flash.each do |name, msg|
+            = content_tag :div, msg, :id => "flash_\#{name}" if msg.is_a?(String)
+        #main{:role => "main"}
+          = yield
+        %footer
+HAML
+        end
+      else
+        # ERB version of default application layout
+        remove_file 'app/views/layouts/application.html.erb'
+        remove_file 'app/views/layouts/application.html.haml'
+        create_file 'app/views/layouts/application.html.erb' do <<-ERB
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>#{app_name}</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <%= stylesheet_link_tag    "application" %>
+  <%= javascript_include_tag "application" %>
+  <%= csrf_meta_tags %>
+</head>
+<body>
+  <div id="container">
+    <header>
+    </header>
+    <div id="main" role="main">
+      <%= yield %>
+    </div>
+    <footer>
+    </footer>
+  </div> <!--! end of #container -->
+</body>
+</html>
+ERB
+        end
+        inject_into_file 'app/views/layouts/application.html.erb', :after => "<header>\n" do
+  <<-ERB
+      <%- flash.each do |name, msg| -%>
+        <%= content_tag :div, msg, :id => "flash_\#{name}" if msg.is_a?(String) %>
+      <%- end -%>
+ERB
+        end
+      end
+    end
+  elsif recipes.include? 'rails 3.0'
+    say_wizard "Not supported for Rails version #{Rails::VERSION::STRING}. HTML5 Boilerplate recipe skipped."
+  else
+    say_wizard "Don't know what to do for Rails version #{Rails::VERSION::STRING}. HTML5 Boilerplate recipe skipped."
+  end
+else
+  recipes.delete('html5')
 end
 
 
@@ -1081,22 +1264,46 @@ ERB
     end
 
     # Add navigation links to the default application layout
-    if recipes.include? 'haml'
-      # There is Haml code in this script. Changing the indentation is perilous between HAMLs.
-      inject_into_file 'app/views/layouts/application.html.haml', :after => "%body\n" do <<-HAML
+    if recipes.include? 'html5'
+      if recipes.include? 'haml'
+        # There is Haml code in this script. Changing the indentation is perilous between HAMLs.
+        inject_into_file 'app/views/layouts/application.html.haml', :after => "%header\n" do <<-HAML
+          %nav
+            %ul
+              = render 'devise/menu/registration_items'
+              = render 'devise/menu/login_items'
+HAML
+        end
+      else
+        inject_into_file 'app/views/layouts/application.html.erb', :after => "<header>\n" do
+  <<-ERB
+      <nav>
+        <ul>
+          <%= render 'devise/menu/registration_items' %>
+          <%= render 'devise/menu/login_items' %>
+        </ul>
+      </nav>
+ERB
+        end
+      end
+    else
+      if recipes.include? 'haml'
+        # There is Haml code in this script. Changing the indentation is perilous between HAMLs.
+        inject_into_file 'app/views/layouts/application.html.haml', :after => "%body\n" do <<-HAML
     %ul.hmenu
       = render 'devise/menu/registration_items'
       = render 'devise/menu/login_items'
 HAML
-      end
-    else
-      inject_into_file 'app/views/layouts/application.html.erb', :after => "<body>\n" do
+        end
+      else
+        inject_into_file 'app/views/layouts/application.html.erb', :after => "<body>\n" do
   <<-ERB
   <ul class="hmenu">
     <%= render 'devise/menu/registration_items' %>
     <%= render 'devise/menu/login_items' %>
   </ul>
 ERB
+        end
       end
     end
 
@@ -1172,6 +1379,27 @@ if config['ban_spiders']
   end
 else
   recipes.delete('ban_spiders')
+end
+
+
+# >--------------------------------[ Extras ]---------------------------------<
+
+@current_recipe = "extras"
+@before_configs["extras"].call if @before_configs["extras"]
+say_recipe 'Extras'
+
+config = {}
+config['footnotes'] = yes_wizard?("Would you like to use 'rails-footnotes' during development?") if true && true unless config.key?('footnotes')
+@configs[@current_recipe] = config
+
+# Application template recipe for the rails_apps_composer. Check for a newer version here:
+# https://github.com/RailsApps/rails_apps_composer/blob/master/recipes/extras.rb
+
+if config['footnotes']
+  say_wizard "Extras recipe running 'after bundler'"
+  gem 'rails-footnotes', '>= 3.7', :group => :development
+else
+  recipes.delete('footnotes')
 end
 
 
