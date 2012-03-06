889c2e88f39d506ef34812ff90a61f65a3346cc6
Daniel Kehoe <daniel@danielkehoe.com>
2012-03-05 18:24:35 -0800

    update application templates using rails_apps_composer 1.1.8

--- rails3-subdomains-template.rb
+++ rails3-subdomains-template.rb
@@ -41,7 +41,7 @@ Rails.application.config.generators do |g|
 end
 RUBY
 
-@recipes = ["haml", "rspec", "cucumber", "guard", "mongoid", "action_mailer", "devise", "add_user", "home_page", "home_page_users", "seed_database", "users_page", "css_setup", "subdomains", "html5", "navigation", "cleanup", "extras", "git"]
+@recipes = ["haml", "rspec", "cucumber", "guard", "mongoid", "action_mailer", "devise", "add_user", "home_page", "home_page_users", "seed_database", "users_page", "subdomains", "html5", "navigation", "cleanup", "extras", "git"]
 
 def recipes; @recipes end
 def recipe?(name); @recipes.include?(name) end
@@ -452,7 +452,7 @@ if config['mongoid']
   say_wizard "REMINDER: When creating a Rails app using Mongoid..."
   say_wizard "you should add the '-O' flag to 'rails new'"
   gem 'bson_ext', '>= 1.5.2'
-  gem 'mongoid', '>= 2.4.4'
+  gem 'mongoid', '>= 2.4.6'
 else
   recipes.delete('mongoid')
 end
@@ -780,6 +780,17 @@ ERB
   # set routes
   gsub_file 'config/routes.rb', /get \"home\/index\"/, 'root :to => "home#index"'
 
+  if recipes.include? 'devise'
+    inject_into_file 'config/routes.rb', :before => "  root :to" do 
+    <<-RUBY
+  authenticated :user do
+    root :to => 'home#index'
+  end
+\n  
+RUBY
+    end
+  end
+
 end
 
 
@@ -870,20 +881,25 @@ FILE
   end
 
   if recipes.include? 'devise'
-    # create a default user
-    append_file 'db/seeds.rb' do <<-FILE
+    if recipes.include? 'devise-confirmable'
+      append_file 'db/seeds.rb' do <<-FILE
+puts 'SETTING UP DEFAULT USER LOGIN'
+user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please', :confirmed_at => DateTime.now
+puts 'New user created: ' << user.name
+FILE
+      end
+    else
+      append_file 'db/seeds.rb' do <<-FILE
 puts 'SETTING UP DEFAULT USER LOGIN'
 user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
 puts 'New user created: ' << user.name
 FILE
-    end
-    if recipes.include? 'devise-confirmable'
-      gsub_file 'db/seeds.rb', /:password_confirmation => 'please'/, ":password_confirmation => 'please', :confirmed_at => DateTime.now"
+      end
     end
   end
-
+  
   run 'bundle exec rake db:seed'
-
+  
 end
 
 
@@ -989,86 +1005,6 @@ ERB
       end
     end
 
-end
-
-
-# >-------------------------------[ CssSetup ]--------------------------------<
-
-@current_recipe = "css_setup"
-@before_configs["css_setup"].call if @before_configs["css_setup"]
-say_recipe 'CssSetup'
-
-
-@configs[@current_recipe] = config
-
-# Application template recipe for the rails_apps_composer. Check for a newer version here:
-# https://github.com/RailsApps/rails_apps_composer/blob/master/recipes/css_setup.rb
-
-after_bundler do
-
-  say_wizard "CssSetup recipe running 'after bundler'"
-
-  # Add a stylesheet with styles for a horizontal menu and flash messages
-  css = <<-CSS
-
-ul.hmenu {
-  list-style: none;
-  margin: 0 0 2em;
-  padding: 0;
-}
-ul.hmenu li {
-  display: inline;
-}
-#flash_notice, #flash_alert {
-  padding: 5px 8px;
-  margin: 10px 0;
-}
-#flash_notice {
-  background-color: #CFC;
-  border: solid 1px #6C6;
-}
-#flash_alert {
-  background-color: #FCC;
-  border: solid 1px #C66;
-}
-
-CSS
-
-  # Add a stylesheet for use with HTML5
-  css_html5 = <<-CSS
-
-header nav ul {
-  list-style: none;
-  margin: 0 0 2em;
-  padding: 0;
-}
-header nav ul li {
-  display: inline;
-}
-#flash_notice, #flash_alert {
-  padding: 5px 8px;
-  margin: 10px 0;
-}
-#flash_notice {
-  background-color: #CFC;
-  border: solid 1px #6C6;
-}
-#flash_alert {
-  background-color: #FCC;
-  border: solid 1px #C66;
-}
-
-CSS
-
-  # rename the application stylesheet to use SCSS
-  copy_file 'app/assets/stylesheets/application.css', 'app/assets/stylesheets/application.css.scss'
-  remove_file 'app/assets/stylesheets/application.css'
-  if recipes.include? 'html5'
-    append_file 'app/assets/stylesheets/application.css.scss', css_html5
-  else
-    append_file 'app/assets/stylesheets/application.css.scss', css
-  end
-  
 end
 
 
@@ -1196,62 +1132,107 @@ end
 say_recipe 'html5'
 
 config = {}
-config['css_option'] = multiple_choice("Which front-end framework would you like for HTML5 and CSS?", [["None", "nothing"], ["Zurb Foundation", "foundation"], ["Twitter Bootstrap", "bootstrap"], ["Skeleton", "skeleton"], ["Just normalize CSS for consistent styling", "normalize"]]) if true && true unless config.key?('css_option')
+config['css_option'] = multiple_choice("Which front-end framework would you like for HTML5 and CSS?", [["None", "nothing"], ["Zurb Foundation", "foundation"], ["Twitter Bootstrap (less)", "bootstrap_less"], ["Twitter Bootstrap (sass)", "bootstrap_sass"], ["Skeleton", "skeleton"], ["Just normalize CSS for consistent styling", "normalize"]]) if true && true unless config.key?('css_option')
 @configs[@current_recipe] = config
 
 # Application template recipe for the rails_apps_composer. Check for a newer version here:
 # https://github.com/RailsApps/rails_apps_composer/blob/master/recipes/html5.rb
 
 case config['css_option']
+
   when 'foundation'
     # https://github.com/zurb/foundation-rails
     gem 'zurb-foundation'
-  when 'bootstrap'
+
+  when 'bootstrap_less'
+    # https://github.com/seyhunak/twitter-bootstrap-rails
+    # http://railscasts.com/episodes/328-twitter-bootstrap-basics
+    gem 'twitter-bootstrap-rails', '>= 2.0.3', :group => :assets
+    recipes << 'bootstrap'
+
+  when 'bootstrap_sass'
     # https://github.com/thomas-mcdonald/bootstrap-sass
     # http://rubysource.com/twitter-bootstrap-less-and-sass-understanding-your-options-for-rails-3-1/
-    gem 'bootstrap-sass', '~> 2.0.1'
+    gem 'bootstrap-sass', '>= 2.0.1'
+    recipes << 'bootstrap'
+
 end
 after_bundler do
   say_wizard "HTML5 recipe running 'after bundler'"
   # add a humans.txt file
-  get "https://raw.github.com/RailsApps/rails3-application-templates/master/files/humans.txt", "public/humans.txt"
+  get 'https://raw.github.com/RailsApps/rails3-application-templates/master/files/humans.txt', 'public/humans.txt'
   # install a front-end framework for HTML5 and CSS3
-  case config['css_option']
-    when 'nothing'
-      say_wizard "no HTML5 front-end framework selected"
-    when 'foundation'
-      say_wizard "installing Zurb Foundation HTML5 framework"
-      insert_into_file "app/assets/javascripts/application.js", "//= require foundation\n", :after => "jquery_ujs\n"
-      insert_into_file "app/assets/stylesheets/application.css.scss", " *= require foundation\n", :after => "require_self\n"
-    when 'bootstrap'
-      say_wizard "installing Twitter Bootstrap HTML5 framework"
-      insert_into_file "app/assets/javascripts/application.js", "//= require bootstrap\n", :after => "jquery_ujs\n"
-      insert_into_file "app/assets/stylesheets/application.css.scss", "\n@import 'bootstrap';\n", :after => "*/\n"
-    when 'skeleton'
-      say_wizard "installing Skeleton HTML5 framework"
-      get "https://raw.github.com/necolas/normalize.css/master/normalize.css", "app/assets/stylesheets/normalize.css.scss"
-      get "https://raw.github.com/dhgamache/Skeleton/master/stylesheets/base.css", "app/assets/stylesheets/base.css.scss"
-      get "https://raw.github.com/dhgamache/Skeleton/master/stylesheets/layout.css", "app/assets/stylesheets/layout.css.scss"
-      get "https://raw.github.com/dhgamache/Skeleton/master/stylesheets/skeleton.css", "app/assets/stylesheets/skeleton.css.scss"
-      get "https://raw.github.com/dhgamache/Skeleton/master/javascripts/tabs.js", "app/assets/javascripts/tabs.js"
-    when 'normalize'
-      say_wizard "normalizing CSS for consistent styling"
-      get "https://raw.github.com/necolas/normalize.css/master/normalize.css", "app/assets/stylesheets/normalize.css.scss"
+  remove_file 'app/assets/stylesheets/application.css'
+  remove_file 'app/views/layouts/application.html.erb'
+  remove_file 'app/views/layouts/application.html.haml'
+  unless recipes.include? 'bootstrap'
+    if recipes.include? 'haml'
+      # Haml version of a simple application layout
+      get 'https://raw.github.com/RailsApps/rails3-application-templates/master/files/simple/views/layouts/application.html.haml', 'app/views/layouts/application.html.haml'
+    else
+      # ERB version of a simple application layout
+      get 'https://raw.github.com/RailsApps/rails3-application-templates/master/files/simple/views/layouts/application.html.erb', 'app/views/layouts/application.html.erb'
+    end
+    # simple css styles
+    get 'https://raw.github.com/RailsApps/rails3-application-templates/master/files/simple/assets/stylesheets/application.css.scss', 'app/assets/stylesheets/application.css.scss'  
+  else # for Twitter Bootstrap
+    if recipes.include? 'haml'
+      # Haml version of a complex application layout using Twitter Bootstrap
+      get 'https://raw.github.com/RailsApps/rails3-application-templates/master/files/twitter-bootstrap/views/layouts/application.html.haml', 'app/views/layouts/application.html.haml'
+    else
+      # ERB version of a complex application layout using Twitter Bootstrap
+      get 'https://raw.github.com/RailsApps/rails3-application-templates/master/files/twitter-bootstrap/views/layouts/application.html.erb', 'app/views/layouts/application.html.erb'
+    end
+    # complex css styles using Twitter Bootstrap
+    get 'https://raw.github.com/RailsApps/rails3-application-templates/master/files/twitter-bootstrap/assets/stylesheets/application.css.scss', 'app/assets/stylesheets/application.css.scss'
   end
-  # Set up the default application layout
   if recipes.include? 'haml'
-    # Haml version of default application layout
-    remove_file 'app/views/layouts/application.html.erb'
-    remove_file 'app/views/layouts/application.html.haml'
-    get "https://raw.github.com/RailsApps/rails3-application-templates/master/files/views/layouts/application.html.haml", "app/views/layouts/application.html.haml"
-    gsub_file "app/views/layouts/application.html.haml", /App_Name/, "#{app_name.humanize.titleize}"
+    gsub_file 'app/views/layouts/application.html.haml', /App_Name/, "#{app_name.humanize.titleize}"
   else
-    # ERB version of default application layout
-    remove_file 'app/views/layouts/application.html.erb'
-    remove_file 'app/views/layouts/application.html.haml'
-    get "https://raw.github.com/RailsApps/rails3-application-templates/master/files/views/layouts/application.html.erb", "app/views/layouts/application.html.erb"
-    gsub_file "app/views/layouts/application.html.erb", /App_Name/, "#{app_name.humanize.titleize}"
+    gsub_file 'app/views/layouts/application.html.erb', /App_Name/, "#{app_name.humanize.titleize}"
   end
+  case config['css_option']
+
+    when 'bootstrap_less'
+      say_wizard 'installing Twitter Bootstrap HTML5 framework (less)'
+      generate 'bootstrap:install'
+      remove_file 'app/assets/stylesheets/application.css' # already created application.css.scss above
+      insert_into_file 'app/assets/stylesheets/bootstrap_and_overrides.css.less', "body { padding-top: 60px; }\n", :after => "@import \"twitter/bootstrap/bootstrap\";\n"
+
+    when 'bootstrap_sass'
+      say_wizard 'installing Twitter Bootstrap HTML5 framework (sass)'
+      insert_into_file 'app/assets/javascripts/application.js', "//= require bootstrap\n", :after => "jquery_ujs\n"
+      create_file 'app/assets/stylesheets/bootstrap_and_overrides.css.scss', <<-RUBY
+// Set the correct sprite paths
+$iconSpritePath: image-path('glyphicons-halflings.png');
+$iconWhiteSpritePath: image-path('glyphicons-halflings-white.png');
+@import "bootstrap";
+body { padding-top: 60px; }
+@import "bootstrap-responsive";
+RUBY
+
+    when 'foundation'
+      say_wizard 'installing Zurb Foundation HTML5 framework'
+      insert_into_file 'app/assets/javascripts/application.js', "//= require foundation\n", :after => "jquery_ujs\n"
+      insert_into_file 'app/assets/stylesheets/application.css.scss', " *= require foundation\n", :after => "require_self\n"
+
+    when 'skeleton'
+      say_wizard 'installing Skeleton HTML5 framework'
+      get 'https://raw.github.com/necolas/normalize.css/master/normalize.css', 'app/assets/stylesheets/normalize.css.scss'
+      get 'https://raw.github.com/dhgamache/Skeleton/master/stylesheets/base.css', 'app/assets/stylesheets/base.css.scss'
+      get 'https://raw.github.com/dhgamache/Skeleton/master/stylesheets/layout.css', 'app/assets/stylesheets/layout.css.scss'
+      get 'https://raw.github.com/dhgamache/Skeleton/master/stylesheets/skeleton.css', 'app/assets/stylesheets/skeleton.css.scss'
+      get 'https://raw.github.com/dhgamache/Skeleton/master/javascripts/tabs.js', 'app/assets/javascripts/tabs.js'
+
+    when 'normalize'
+      say_wizard 'normalizing CSS for consistent styling'
+      get 'https://raw.github.com/necolas/normalize.css/master/normalize.css', 'app/assets/stylesheets/normalize.css.scss'
+
+    when 'nothing'
+      say_wizard 'no HTML5 front-end framework selected'
+
+  end
+
 end
 
 
@@ -1449,11 +1430,15 @@ after_bundler do
   gsub_file "README", /App_Name/, "#{app_name.humanize.titleize}"
   gsub_file "README.textile", /App_Name/, "#{app_name.humanize.titleize}"
 
-  # remove commented lines from Gemfile
+  # remove commented lines and multiple blank lines from Gemfile
   # thanks to https://github.com/perfectline/template-bucket/blob/master/cleanup.rb
-  gsub_file "Gemfile", /#.*\n/, "\n"
-  gsub_file "Gemfile", /\n+/, "\n"
+  gsub_file 'Gemfile', /#.*\n/, "\n"
+  gsub_file 'Gemfile', /\n^\s*\n/, "\n"
 
+  # remove commented lines and multiple blank lines from config/routes.rb
+  gsub_file 'config/routes.rb', /  #.*\n/, "\n"
+  gsub_file 'config/routes.rb', /\n^\s*\n/, "\n"
+  
 end
 
 
