9313f590b0f9b1e4316fc457610efd27e9a1fcaa
Daniel Kehoe <daniel@danielkehoe.com>
2012-07-22 10:13:38 -0700

    remove files not needed by rails_apps_composer v2.0

--- files-v2/app/controllers/profiles_controller-subdomains.rb
+++
@@ -1,9 +0,0 @@
-class ProfilesController < ApplicationController
-  def show
-    @user = User.first(conditions: { name: request.subdomain }) || not_found
-  end
-  
-  def not_found
-    raise ActionController::RoutingError.new('User Not Found')
-  end
-end
